ec7c554adf02c68c3dd6c8eba25caada8b21817d
Xarfex <xarfex@mail.ru>
2023-07-19 00:09:59 +0700

    test for ssh_key

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -2,14 +2,13 @@
 import os
 from pathlib import Path
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['valerykittygram.hopto.org', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
